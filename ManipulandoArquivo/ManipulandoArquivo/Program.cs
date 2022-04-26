c17350cc2a20f02edfae3e2f31e17f0ed901143d
Danyllo Fellip Moura da Silva <danyllo.silva@detran.pe.gov.br>
2022-04-26 09:05:33 -0300

    first commit

---
+++ ManipulandoArquivo/ManipulandoArquivo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ManipulandoArquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filePath = @"C:\danyllo\repositorio\ManipulandoArquivo\meuarquivo.txt";
+
+            //Cria Arquico
+            //File.Create(filePath);
+
+            using var file = File.AppendText(filePath);
+
+            file.WriteLine("Teste");
+            file.Close();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
